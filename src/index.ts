648f041723bd3fb0eb22e4f7f4c6fa713d82b20f
Gabriel Lemos <glemoss@yahoo.com>
2024-04-25 09:28:52 -0300

    add multer config and upload route

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,21 @@
 import express from 'express';
 import multer from 'multer';
+import path from 'path';
 
+import { storage } from '../multerConfig'
+
+const upload = multer({ storage: storage });
 const app = express();
 
-app.listen(3000);
+const PORT = parseInt(process.env.PORT || '3000', 10) ;
+const HOST = process.env.HOST || '0.0.0.0';
+
+app.use('uploads', express.static(path.resolve('../uploads')));
+
+app.post('/upload', upload.single('file'), (req, res) => {
+    return res.json(req.file?.filename);
+});
+
+app.listen(PORT, HOST, () => {
+    console.log(`Running on http://${HOST}:${PORT}`);
+});
