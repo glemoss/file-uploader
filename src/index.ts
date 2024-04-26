1f11ba730315c8c65eba0881cef0bff0fe046ab2
Gabriel Lemos <glemoss@yahoo.com>
2024-04-26 10:52:06 -0300

    set status code and remove build files

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,7 @@ const HOST = process.env.HOST || '0.0.0.0';
 app.use('uploads', express.static(path.resolve('../uploads')));
 
 app.post('/upload', upload.single('file'), (req, res) => {
+    res.statusCode = 200;
     return res.json(req.file?.filename);
 });
 
