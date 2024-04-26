169b973780e5bfe3d008befc01addd10e8ec4768
Gabriel Lemos <glemoss@yahoo.com>
2024-04-26 09:58:13 -0300

    ts build

--- src/index.js
+++ src/index.js
@@ -4,5 +4,18 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const multer_1 = __importDefault(require("multer"));
+const path_1 = __importDefault(require("path"));
+const multerConfig_1 = require("../multerConfig");
+const upload = (0, multer_1.default)({ storage: multerConfig_1.storage });
 const app = (0, express_1.default)();
-app.listen(3000);
+const PORT = parseInt(process.env.PORT || '3000', 10);
+const HOST = process.env.HOST || '0.0.0.0';
+app.use('uploads', express_1.default.static(path_1.default.resolve('../uploads')));
+app.post('/upload', upload.single('file'), (req, res) => {
+    var _a;
+    return res.json((_a = req.file) === null || _a === void 0 ? void 0 : _a.filename);
+});
+app.listen(PORT, HOST, () => {
+    console.log(`Running on http://${HOST}:${PORT}`);
+});
